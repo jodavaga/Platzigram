6bf4161bfec04d139d017169654d8464c4a7ae9e
Jose D. Vasquez <jodavaga@gmail.com>
2017-09-26 14:20:38 -0500

    Se crea la tarea para manejar scripts, pasandolos por beowserify

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 gulp.task('styles', function(){
     gulp
@@ -10,11 +12,18 @@ gulp.task('styles', function(){
         .pipe(gulp.dest('public'))
 });
 
-
 gulp.task('assets', function(){
     gulp
         .src('assets/*')
         .pipe(gulp.dest('public'));
 });
 
-gulp.task('default', ['styles', 'assets']);
+gulp.task('scripts', function(){
+    browserify('./src/index.js')
+        .bundle()
+        .pipe(source('index.js'))
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('default', ['styles', 'assets', 'scripts']);
