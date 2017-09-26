7cccd3c15c577863752d402fc28b9ca4ddfb273f
Jose D. Vasquez <jodavaga@gmail.com>
2017-09-26 13:37:12 -0500

    Proyecto inicializado. bundle con Gulp, organizacion de archivos y assets

---
+++ server.js
@@ -0,0 +1,21 @@
+//Define la variable que se va usar con Expresss, busca el modulo
+var express = require('express');
+//se guarda el 'main' de expree en la variable app
+var app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  //cuando entre al index, le muestra al cliente 'Hola Mundo'
+  res.render('index');
+})
+
+//Se inicializa el puerto para que escuche por el puerto 3000
+app.listen(3000, function (err) {
+  if (err) return console.log('Hubo un error'), process.exit(1);
+
+  console.log('Platzigram escuchando en el puerto 3000');
+})
